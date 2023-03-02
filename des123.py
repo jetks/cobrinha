7c1a3e3c2d92d4810e769cd8a52bafb435bb06db
Jéssica Tolksdorf <93948391+jetks@users.noreply.github.com>
2023-03-02 19:09:12 -0300

    Update des123.py

--- des123.py
+++ des123.py
@@ -1,2 +1,20 @@
+#1 crie um script que leia o nome de uma pessoa e mostra uma mensagem de boas vindas de volta de acordo com o valor digitado
 nome = input("Qual é o seu nome?  ")
-print("bem vindo, ", nome)
+print("Bem vindo, ", nome)
+
+
+
+#2 crie um script que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+dia = input("Digite a data do seu nascimento: ")
+mes = input("Em que mês você nasceu? ")
+ano = input("Qual ano você nasceu? ")
+
+print('Você nasceu no dia '+dia+' de '+mes+' de '+ano+'. Correto?')
+
+
+#3 crie um script que leia dois numeros e tente mostrar a soma entre eles
+num1 = int(input("valor 1: "))
+num2 = int(input("valor 2: "))
+soma = num1 + num2
+
+print(soma)
